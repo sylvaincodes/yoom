70aaf3b010dc0961de58bac54755c3cb944496fd
sylvaincodes <sylvaincodeur@gmail.com>
2024-04-12 23:23:07 +0100

    delete error component

--- app/(pages)/personalroom/page.tsx
+++ app/(pages)/personalroom/page.tsx
@@ -1,6 +1,5 @@
 import { Container } from "@/components/ui/container";
 import { Heading } from "@/components/ui/heading";
-import { MeetingUp } from "@/components/wrappers/meeting/meeting";
 import { Personnal } from "@/components/wrappers/personnalinfo/personnal";
 import { mergeOpenGraph } from "@/lib/mergeOpenGraph";
 import { Metadata } from "next";
@@ -8,10 +7,10 @@ import { Metadata } from "next";
 export default function Page() {
   return (
     <section className="pt-24 bg-dark-1 flex-1 text-white min-h-screen">
-        <Container>
-           <Heading title="Ma salle personnel des réunions"/>
-           <Personnal />
-        </Container>
+      <Container>
+        <Heading title="Ma salle personnel des réunions" />
+        <Personnal />
+      </Container>
     </section>
   );
 }
@@ -22,9 +21,9 @@ export const metadata: Metadata = {
   icons: {
     icon: "/logo/logo.png",
   },
-  
+
   openGraph: mergeOpenGraph({
     title: "YOOM - Salle personnel",
-    url: '/meeting/personalroom',
+    url: '/personalroom',
   }),
 };
